ff3153aab661bf71a85f0b55f1a452b40d791d1e
belennhr <belenn.hr@gmail.com>
2025-03-10 14:25:23 +0100

    bookScript

--- DCDGLecture3aEmpty/Assets/Scripts/bookScript.cs
+++ DCDGLecture3aEmpty/Assets/Scripts/bookScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class bookScript : MonoBehaviour
 {
+    [SerializeField] Transform camTransform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class bookScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Physics.Raycast(camTransform.position, camTransform.forward, out hitinfo, 2) && hitinfo.collider.CompareTag("Book"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("notas");
+            }
+        }
     }
 }
